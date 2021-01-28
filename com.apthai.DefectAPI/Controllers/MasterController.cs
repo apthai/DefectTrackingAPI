8538972ddacfe20a05bdcaa8b850a66fa32ee5fc
Criffer <pacharavach@gmail.com>
2021-01-28 17:58:35 +0700

    deploy

--- com.apthai.DefectAPI/Controllers/MasterController.cs
+++ com.apthai.DefectAPI/Controllers/MasterController.cs
@@ -852,7 +852,7 @@ namespace com.apthai.DefectAPI.Controllers
                     a.Resource = new List<PicInDetailObj>();
                     return new
                     {
-                        success = true,
+                        success = false,
                         data = a,
                         message = "Cannot Find Any Defect Header!"
                     };
